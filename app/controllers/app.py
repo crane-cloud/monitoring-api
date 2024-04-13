9208e1f043a02adbb825c6be75c51df8c5fdef5c
rhodinemma <rhodinemma10@gmail.com>
2024-04-13 23:13:38 +0300

    feat: functionality to retrieve app memory, cpu and network metrics

---
+++ app/controllers/app.py
@@ -0,0 +1,122 @@
+import json
+from prometheus_http_client import Prometheus
+from flask_restful import Resource, request
+
+from app.helpers.authenticate import (
+    jwt_required
+)
+from app.helpers.utils import get_app_data
+
+class AppMemoryUsageView(Resource):
+    @jwt_required
+    def post(self, project_id, app_id):
+        app = get_app_data(project_id, app_id, request)
+
+        if app.status_code != 200:
+            return dict(status='fail', message=app.message), app.status_code
+
+        start = app.start
+        end = app.end
+        step = app.step
+        app_alias = app.app_alias
+        namespace = app.namespace
+        prometheus = Prometheus()
+
+        try:
+            prom_memory_data = prometheus.query_rang(
+                start=start,
+                end=end,
+                step=step,
+                metric='sum(rate(container_memory_usage_bytes{container_name!="POD", image!="",pod=~"' + app_alias + '.*", namespace="' + namespace + '"}[5m]))')
+        except Exception as error:
+            return dict(status='fail', message=str(error)), 500
+
+        new_data = json.loads(prom_memory_data)
+        final_data_list = []
+
+        try:
+            for value in new_data["data"]["result"][0]["values"]:
+                mem_case = {'timestamp': float(
+                    value[0]), 'value': float(value[1])}
+                final_data_list.append(mem_case)
+        except:
+            return dict(status='fail', message='No values found'), 404
+
+        return dict(status='success', data=dict(values=final_data_list)), 200
+
+
+class AppCpuUsageView(Resource):
+    @jwt_required
+    def post(self, project_id, app_id):
+        app = get_app_data(project_id, app_id, request)
+
+        if app.status_code != 200:
+            return dict(status='fail', message=app.message), app.status_code
+
+        start = app.start
+        end = app.end
+        step = app.step
+        app_alias = app.app_alias
+        namespace = app.namespace
+        prometheus = Prometheus()
+
+        try:
+            prom_cpu_data = prometheus.query_rang(
+                start=start,
+                end=end,
+                step=step,
+                metric='sum(rate(container_cpu_usage_seconds_total{container!="POD", image!="", namespace="' +
+                namespace + '", pod=~"' + app_alias + '.*"}[5m]))')
+        except Exception as error:
+            return dict(status='fail', message=str(error)), 500
+
+        new_data = json.loads(prom_cpu_data)
+        final_data_list = []
+
+        try:
+            for value in new_data["data"]["result"][0]["values"]:
+                case = {'timestamp': float(value[0]), 'value': float(value[1])}
+                final_data_list.append(case)
+        except:
+            return dict(status='fail', message='No values found'), 404
+
+        return dict(status='success', data=dict(values=final_data_list)), 200
+
+
+class AppNetworkUsageView(Resource):
+    @jwt_required
+    def post(self, project_id, app_id):
+        app = get_app_data(project_id, app_id, request)
+
+        if app.status_code != 200:
+            return dict(status='fail', message=app.message), app.status_code
+
+        start = app.start
+        end = app.end
+        step = app.step
+        app_alias = app.app_alias
+        namespace = app.namespace
+        prometheus = Prometheus()
+
+        try:
+            prom_net_data = prometheus.query_rang(
+                start=start,
+                end=end,
+                step=step,
+                metric='sum(rate(container_network_receive_bytes_total{namespace="' +
+                namespace + '", pod=~"' + app_alias + '.*"}[5m]))'
+            )
+        except Exception as error:
+            return dict(status='fail', message=str(error)), 500
+
+        new_data = json.loads(prom_net_data)
+        final_data_list = []
+
+        try:
+            for value in new_data["data"]["result"][0]["values"]:
+                case = {'timestamp': float(value[0]), 'value': float(value[1])}
+                final_data_list.append(case)
+        except:
+            return dict(status='fail', message='No values found'), 404
+
+        return dict(status='success', data=dict(values=final_data_list)), 200
--- app/helpers/utils.py
+++ app/helpers/utils.py
@@ -6,6 +6,7 @@ from types import SimpleNamespace
 
 PRODUCT_BASE_URL = os.getenv('PRODUCT_BASE_URL')
 PROJECT_ENDPOINT = f"{PRODUCT_BASE_URL}/projects"
+APP_ENDPOINT = f"{PRODUCT_BASE_URL}/apps"
 
 
 def get_project_data(project_id, request):
@@ -49,6 +50,69 @@ def get_project_data(project_id, request):
         start=start,
         end=end,
         step=step,
+        namespace=namespace,
+        status_code=200
+    )
+
+
+def get_app_data(project_id, app_id, request):
+    app_query_data = request.get_json()
+
+    metric_schema = MetricsSchema()
+
+    validated_query_data = metric_schema.load(
+        app_query_data)
+
+    # if errors:
+    #   return dict(status='fail', message=errors), 400
+
+    current_time = datetime.datetime.now()
+    yesterday_time = current_time + datetime.timedelta(days=-1)
+
+    start = validated_query_data.get('start', yesterday_time.timestamp())
+    end = validated_query_data.get('end', current_time.timestamp())
+    step = validated_query_data.get('step', '1h')
+
+    # get project details
+    project_response = requests.get(
+        f"{PROJECT_ENDPOINT}/{project_id}", headers={
+            "accept": "application/json",
+            "Authorization": request.headers.get('Authorization')
+        })
+
+    if not project_response.ok:
+        return SimpleNamespace(status='failed', message="Failed to fetch project for current user", status_code=400)
+
+    project_response = project_response.json()
+    project_data = project_response['data']['project']
+
+    # get app details
+    app_response = requests.get(
+        f"{APP_ENDPOINT}/{app_id}", headers={
+            "accept": "application/json",
+            "Authorization": request.headers.get('Authorization')
+        }
+    )
+
+    if not app_response.ok:
+        return SimpleNamespace(status='failed', message="Failed to fetch app for current user", status_code=400)
+
+    app_response = app_response.json()
+    app_data = app_response['data']['apps']
+
+    app_alias = app_data['alias']
+    namespace = project_data['alias']
+
+    prometheus_url = project_data['cluster']['prometheus_url']
+    if not prometheus_url:
+        return SimpleNamespace(status='fail', message='No prometheus url provided', status_code=404)
+
+    os.environ["PROMETHEUS_URL"] = prometheus_url
+    return SimpleNamespace(
+        start=start,
+        end=end,
+        step=step,
+        app_alias=app_alias,
         namespace=namespace,
         status_code=200
     )
