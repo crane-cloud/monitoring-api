9b0c1c7e477debb377504872f6ce73fdaa886c0e
Mubangizi <mubangizia22@gmail.com>
2024-05-09 03:56:56 +0300

    Dockerfile able to start from script file

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
 # source .env
 
 # apply migrations onto db
-# flask db upgrade
+flask db upgrade
 
 # start server
-flask run --host=0.0.0.0 --port=50000
+flask run --host=0.0.0.0 --port=5000
